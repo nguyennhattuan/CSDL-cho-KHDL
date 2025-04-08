f88f7a3106575f7a39d6c778d737b2fd4ed1b637
nguyennhattuan <119574081+nguyennhattuan@users.noreply.github.com>
2025-04-08 17:15:44 +0700

    Delete chuong_3 directory

--- chuong_3/47_NguyenNhatTuan.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-    (COUNT(*) * SUM(A * B) - SUM(A) * SUM(B)) / 
-    (SQRT(COUNT(*) * SUM(A * A) - SUM(A) * SUM(A)) * 
-    SQRT(COUNT(*) * SUM(B * B) - SUM(B) * SUM(B))) AS correlation_coefficient
-FROM 
-    your_table_name;
